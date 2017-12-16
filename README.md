013fb06510828c6dc171c173f35af39f3b19e15e
Nicolas Jalibert <NJalibert(@infotrafic.com>
2017-12-16 13:32:05 +0100

    add the OLED screen

---
+++ README.md
@@ -0,0 +1,38 @@
+# No early wake-up
+Never accept a child to wake you up too early in the morning
+
+## Description
+Your child should know if he/she can walk to your bedroom or if they have to keep calm and try to sleep again in his/her bedroom.
+A child isn't aware of time reading and has _very_ limited time notions
+Now with this a few pieces of electronics and an arduino, they will be able to know if it's time or not to slip into your room.
+
+## Requirements
+* Arduino uno (may work with other harware)
+* DS1307 clock
+* SSD1306 OLED screen 128x32 (bi-color)
+* 220Ω resistor
+* LED 
+
+
+## Cable
+| Arduino | <=> | <=> | Clock |
+| :---: | :---: | :---: | :---: |
+| 5V | <= | => | 5V |
+| Gnd | <= | => | Gnd |
+| A4 | <= | => | SDA |
+| A5 | <= | => | SCL |
+
+
+| Arduino | <=> | <=> | LED |
+| :---: | :---: | :---: | :---: |
+| D13 | <= | 220Ω |  |
+|    | 220Ω | LED+ |  |
+|  |  | LED- | Gnd |
+
+| Arduino | <=> | <=> | OLED |
+| :---: | :---: | :---: | :---: |
+| 3.3V | <= | => | VCC |
+| Gnd | <= | => | Gnd |
+| A4 | <= | => | SDA |
+| A5 | <= | => | SCL |
+
--- reveil_arduino.ino
+++ reveil_arduino.ino
@@ -4,8 +4,13 @@
  */
 
 /* Dépendances */
+#include <SPI.h>
 #include <Wire.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
 #include "DS1307.h"
+
+#define OLED_RESET 4
 
 #define DEBUG 1
 
@@ -19,6 +24,9 @@
 #endif
 
 int boardLed = 13;
+int externLed = 8;
+Adafruit_SSD1306 display(OLED_RESET);
+
 
 /** Fonction de conversion BCD -> decimal */
 byte bcd_to_decimal(byte bcd) {
@@ -83,10 +91,40 @@ void adjust_current_datetime(DateTime_t *datetime) {
   Wire.endTransmission(); // Fin de transaction I2C
 }
 
+void blinkSequence(int ledPin){
+  for(int a=0; a<5; a++){
+    digitalWrite(ledPin, HIGH);
+    delay(400);
+    for(int b=0; b<a ; b++){
+      digitalWrite(ledPin, LOW);
+      delay(50 - a);
+      digitalWrite(ledPin, HIGH);
+      delay(50 - a);
+    }
+  }
+  digitalWrite(ledPin, LOW);
+}
+
+void oled_night(){
+  display.clearDisplay();
+  display.display();
+}
+
+
+void oled_sunrise(void) {
+  for (int16_t i=0; i<display.width()/2; i+=3) {
+    display.drawCircle(display.width()/2, 7, i, WHITE);
+  }
+  display.fillRect(0, 8, display.width(), display.height(), 0);
+  display.display();
+  //delay(1000);
+}
+
 
 /** Fonction setup() */
 void setup() {
   pinMode(boardLed, OUTPUT);
+  pinMode(externLed, OUTPUT);
   /* Initialise le port série */
   if(DEBUG){
     Serial.begin(115200);
@@ -99,20 +137,38 @@ void setup() {
   /* Vérifie si le module RTC est initialisé */
   DateTime_t now;
   if (read_current_datetime(&now)) {
+  //if (1) {
     if(DEBUG){
       Serial.println(F("L'horloge du module RTC n'est pas active !"));
     }
     // Reconfiguration avec une date et heure en dure (pour l'exemple)
-    now.seconds = 0;
-    now.minutes = 33;
+    now.seconds = 2;
+    now.minutes = 0;
     now.hours = 19; // 12h 0min 0sec
     now.is_pm = 0; 
-    now.day_of_week = 3;
-    now.days = 6; 
+    now.day_of_week = 1;
+    now.days = 11;
     now.months = 12;
     now.year = 17; // 1 dec 2016
     adjust_current_datetime(&now);
   }
+  if(DEBUG){
+    Serial.println(F("Sequence d'initialisation des LED"));
+  }
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  // init done
+
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  // Clear the buffer.
+  display.clearDisplay();
+  display.display();
+
+  blinkSequence(boardLed);
+  blinkSequence(externLed);
 }
 
 /* Retourne l'heure et les minutes au format (100*Heures + Minutes) */
@@ -120,7 +176,7 @@ int wakeUpTimeByDay(int dayOfWeek){
     switch(dayOfWeek){
     case 0://Dimanche
     case 7:
-      return 830;
+      return 730;
       break;
     case 1://Lundi
       return 645;
@@ -138,7 +194,7 @@ int wakeUpTimeByDay(int dayOfWeek){
       return 710;
       break;
     case 6://Samedi
-      return 800;
+      return 730;
       break;
   }
 }
@@ -147,7 +203,7 @@ int isWakeUpAllowed(int dayOfWeek, int nowHour, int nowMinute){
   int strangeFormat=(100*nowHour)+nowMinute;
   Serial.println(strangeFormat);
 
-  if(strangeFormat > 1200){
+  if(strangeFormat > 1302){
     Serial.println(F("Il est trop tard pour allumer"));
     return 0;
   }else if(strangeFormat < wakeUpTimeByDay(dayOfWeek)){
@@ -185,9 +241,13 @@ void loop() {
       Serial.print(F(":"));
       Serial.println(now.seconds);
     if(isWakeUpAllowed(now.day_of_week, now.hours, now.minutes)){
-      digitalWrite(boardLed, HIGH);   // turn the LED on (HIGH is the voltage level)
+      //digitalWrite(boardLed, LOW);   // turn the LED on (HIGH is the voltage level)
+      digitalWrite(externLed, HIGH);   // turn the LED on (HIGH is the voltage level)
+      oled_sunrise();
     }else{
-      digitalWrite(boardLed, LOW);    // turn the LED off by making the voltage LOW
+      //digitalWrite(boardLed, HIGH);    // turn the LED off by making the voltage LOW
+      digitalWrite(externLed, LOW);    // turn the LED off by making the voltage LOW
+      oled_night();
     }
   }
   
